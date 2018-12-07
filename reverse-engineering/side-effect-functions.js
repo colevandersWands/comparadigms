0c7ddc2eff20b2aab22a9365580ee399d57c8812
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2018-12-07 09:48:23 +0100

    license

--- reverse-engineering/side-effect-functions.js
+++ reverse-engineering/side-effect-functions.js
@@ -1,1 +1,13 @@
-side-effect-functions.js
+  Structured programming is based around data structures and subroutines.
+
+  The subroutines are where the recipe is implemented, and the data structures are simply containers for the information needed by those subroutines.
+
+  OOP shifts the primary focus to the data itself.
+
+  Procedural: "what do I want to do and what will I need to know to do it"
+
+  OOP: "what kind of things do I need to have and what can those things do for me".
+
+  Procedural: functions first and then devise data structures to support them.
+
+  OOP: design types first and then the operations needed to work with them.side-effect-functions.js
